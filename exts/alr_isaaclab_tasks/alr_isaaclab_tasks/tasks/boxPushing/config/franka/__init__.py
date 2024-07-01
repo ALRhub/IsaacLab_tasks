41828d6cbb357ba0dbb82866e8f0ae9c27e34225
Johann Bonneau <johann.bonneau2000@gmail.com>
2024-07-01 14:25:54 +0200

    added config folder for the box pushing task

---
+++ exts/alr_isaaclab_tasks/alr_isaaclab_tasks/tasks/boxPushing/config/franka/__init__.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2022-2024, The ORBIT Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+
+import gymnasium as gym
+
+import fancy_gym.envs.registry as fancy_gym_registry
+
+from . import agents, joint_pos_env_cfg
+
+##
+# Register Gym environments.
+##
+
+##
+# Joint Position Control
+##
+
+# Dense reward
+for reward_type in ["Dense", "TemporalSparse"]:
+    for rsl_rl_cfg_name in ["Step_RL_Orbit_HP", "Step_RL_Fancy_Gym_HP"]:
+        gym.register(
+            id=f"Isaac-Box-Pushing-{reward_type}-{rsl_rl_cfg_name}-Franka-v0",
+            entry_point="omni.isaac.contrib_tasks.boxPushing.box_pushing_env:BoxPushingEnv",
+            kwargs={
+                "env_cfg_entry_point": getattr(joint_pos_env_cfg, f"FrankaBoxPushingEnvCfg_{reward_type}"),
+                "rsl_rl_cfg_entry_point": getattr(agents.rsl_rl_cfg, f"BoxPushingPPORunnerCfg_{rsl_rl_cfg_name}"),
+            },
+            disable_env_checker=True,
+        )
