98bdf61c208d14e1e24878eb669e732088bda843
Johann Bonneau <johann.bonneau2000@gmail.com>
2024-07-01 15:47:26 +0200

    finalized mirgation to isaac lab for the box pushing task

--- exts/alr_isaaclab_tasks/alr_isaaclab_tasks/tasks/boxPushing/config/franka/__init__.py
+++ exts/alr_isaaclab_tasks/alr_isaaclab_tasks/tasks/boxPushing/config/franka/__init__.py
@@ -6,8 +6,6 @@
 
 import gymnasium as gym
 
-import fancy_gym.envs.registry as fancy_gym_registry
-
 from . import agents, joint_pos_env_cfg
 
 ##
@@ -23,7 +21,7 @@ for reward_type in ["Dense", "TemporalSparse"]:
     for rsl_rl_cfg_name in ["Step_RL_Orbit_HP", "Step_RL_Fancy_Gym_HP"]:
         gym.register(
             id=f"Isaac-Box-Pushing-{reward_type}-{rsl_rl_cfg_name}-Franka-v0",
-            entry_point="omni.isaac.contrib_tasks.boxPushing.box_pushing_env:BoxPushingEnv",
+            entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
             kwargs={
                 "env_cfg_entry_point": getattr(joint_pos_env_cfg, f"FrankaBoxPushingEnvCfg_{reward_type}"),
                 "rsl_rl_cfg_entry_point": getattr(agents.rsl_rl_cfg, f"BoxPushingPPORunnerCfg_{rsl_rl_cfg_name}"),
