33059ee2e62be2bbd6fe38e2530446dee4c58d9d
Johann Bonneau <johann.bonneau2000@gmail.com>
2024-09-12 19:25:34 +0200

    Set hyperparameters

--- exts/alr_isaaclab_tasks/alr_isaaclab_tasks/tasks/stackCube/config/franka/agents/rsl_rl_cfg.py
+++ exts/alr_isaaclab_tasks/alr_isaaclab_tasks/tasks/stackCube/config/franka/agents/rsl_rl_cfg.py
@@ -14,11 +14,11 @@ from omni.isaac.lab_tasks.utils.wrappers.rsl_rl import (
 
 @configclass
 class StackCubePPORunnerCfg(RslRlOnPolicyRunnerCfg):
-    num_steps_per_env = 24
-    max_iterations = 1500
+    num_steps_per_env = 50
+    max_iterations = 1000
     save_interval = 50
     experiment_name = "franka_stack"
-    empirical_normalization = False
+    empirical_normalization = True
     policy = RslRlPpoActorCriticCfg(
         init_noise_std=1.0,
         actor_hidden_dims=[256, 128, 64],
@@ -26,16 +26,16 @@ class StackCubePPORunnerCfg(RslRlOnPolicyRunnerCfg):
         activation="elu",
     )
     algorithm = RslRlPpoAlgorithmCfg(
-        value_loss_coef=1.0,
+        value_loss_coef=0.5,
         use_clipped_value_loss=True,
         clip_param=0.2,
-        entropy_coef=0.006,
-        num_learning_epochs=5,
-        num_mini_batches=4,
-        learning_rate=1.0e-4,
-        schedule="adaptive",
-        gamma=0.98,
-        lam=0.95,
-        desired_kl=0.01,
-        max_grad_norm=1.0,
+        entropy_coef=0.0,
+        num_learning_epochs=8,
+        num_mini_batches=32,
+        learning_rate=3e-4,
+        schedule="fixed",
+        gamma=0.8,
+        lam=0.9,
+        desired_kl=0.1,
+        max_grad_norm=0.5,
     )
