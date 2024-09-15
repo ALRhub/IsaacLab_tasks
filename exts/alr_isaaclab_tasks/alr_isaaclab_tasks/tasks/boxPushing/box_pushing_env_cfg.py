d0ba4246f94df3d3626a99858d1f0947054fce9b
Johann Bonneau <johann.bonneau2000@gmail.com>
2024-09-15 18:11:44 +0200

    Added random yaw initbox pose

--- exts/alr_isaaclab_tasks/alr_isaaclab_tasks/tasks/boxPushing/box_pushing_env_cfg.py
+++ exts/alr_isaaclab_tasks/alr_isaaclab_tasks/tasks/boxPushing/box_pushing_env_cfg.py
@@ -140,7 +140,7 @@ class EventCfg:
         func=mdp.sample_box_poses,
         mode="reset",
         params={
-            "pose_range": {"x": (-0.15, 0.15), "y": (-0.45, 0.45), "z": (0.0, 0.0)},
+            "pose_range": {"x": (-0.15, 0.15), "y": (-0.45, 0.45), "z": (0.0, 0.0), "yaw": (0.0, 2 * torch.pi)},
             "asset_cfg": SceneEntityCfg("object", body_names="Object"),
         },
     )
@@ -196,7 +196,7 @@ class TemporalSparseRewardCfg:  # TODO set weights
         weight=-2.0,
     )
 
-    energy_cost = RewTerm(func=mdp.action_l2, weight=-5e-4)
+    energy_cost = RewTerm(func=mdp.action_scaled_l2, weight=-5e-4)
 
     joint_position_limit = RewTerm(func=mdp.joint_pos_limits_bp, weight=-1.0)
 
